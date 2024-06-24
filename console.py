c76f6d42ba0e2ed3561c6517c4bf14be20ed3dbf
Sandy Obare <obaresandy@gmail.com>
2024-06-25 02:10:20 +0300

    do_create() function updates to allow for object creation with given parameters: * Command syntax: create <Class name> <param 1> <param 2> <param 3>... * Param syntax: <key name>=<value> * Value syntax:   * String: "<value>" => starts with a double quote     * any double quote inside the value must be escaped with a backslash \     * all underscores _ must be replace by spaces . Example: You want to set the string My little house to the attribute name, your command line must be name="My_little_house"   * Float: <unit>.<decimal> => contains a dot .   * Integer: <number> => default case * If any parameter doesn’t fit with these requirements or can’t be recognized correctly by your program, it must be skipped Minor code improvements

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,10 +118,35 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        params = args.split(' ')
+        model = params[0]
+        if params[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        if len(params) > 1:
+            kwargs = {}
+
+            for param in params[1:]:
+                name_val = param.split('=', 1)
+                if len(name_val) < 2:
+                    continue
+                if name_val[1].startswith('"'):
+                    val = name_val[1][1:-1].replace('_', ' ').replace('"', '\\"')
+                elif '.' in name_val[1]:
+                    try:
+                        val = float(name_val[1])
+                    except ValueError:
+                        continue
+                else:
+                    try:
+                        val = int(name_val[1])
+                    except ValueError:
+                        continue
+                if val:
+                    kwargs.update({name_val[0]: val})
+            new_instance = HBNBCommand.classes[model](**kwargs)
+        else:
+            new_instance = HBNBCommand.classes[model]()
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +297,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +305,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
