9e43528627cb8c7b203e377db4636c01e998249d
Sandy Obare <obaresandy@gmail.com>
2024-06-25 02:21:19 +0300

    - Provisions for unspecified 'id', 'created_at', and 'updated_at' kwargs argument during object creation

--- models/base_model.py
+++ models/base_model.py
@@ -15,12 +15,23 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            from models import storage
+            if 'id' not in kwargs:
+                kwargs.update({'id': str(uuid.uuid4())})
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs.update({'updated_at': datetime.now()})
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs.update({'created_at': datetime.now()})
+            if '__class__' in kwargs:
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
+            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
