ce0e4ea506cdfe15ff2a420488f56c3d48652afb
Sandy Obare <obaresandy@gmail.com>
2024-06-26 18:48:07 +0300

    Update State: (models/state.py) * Addition of Base as superclass to State after BaseModel * Addition of class attribute '__tablename__' to represent states table * Modificatiion of class attriute 'name' to represent column in states table * Addition of class attribute cities to represent relationship between states table   and cities table * Addition of 'cities' getter attribute that returns list of city instances with state_id   equal to the current State.id

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state',
+                          cascade='all, delete-orphan')
+
+    @property
+    def cities(self):
+        from models import storage as fs
+        from models.city import City
+
+        cty_objs = fs.all(City)
+        state_cities = []
+        for value in cty_objs.values():
+            if value['state_id'] == self.id:
+                state_cities.append(value)
+        return state_cities
