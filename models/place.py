f900e28d8dcbf852ab02982f8998ecc8b17f2fb5
Sandy Obare <obaresandy@gmail.com>
2024-06-27 03:50:17 +0300

    * Addition of Base class as superclass of Place after BaseModel * Addition/modification of class attributes to represent places table and columns * Addition of association table 'place_amenity' to represent relationship between   places and amenities tables * Public instance methods: getters(reviews and amenities) and setter(amenities)

--- models/place.py
+++ models/place.py
@@ -1,18 +1,67 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+metadata = Base.metadata
+
+place_amenity = Table(
+    'place_amenity',
+    metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True,
+           nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'),
+           primary_key=True, nullable=False))
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", backref='place',
+                           cascade='all, delete-orphan')
+    amenities = relationship("place_amenity", secondary=place_amenity,
+                             viewonly=False)
     amenity_ids = []
+
+    @property
+    def reviews(self):
+        from models import storage as fs
+        from models.review import Review
+
+        review_objs = fs.all(Review)
+        place_reviews = []
+        for value in review_objs.values():
+            if value.place_id == self.id:
+                place_reviews.append(value)
+        return place_reviews
+
+    @property
+    def amenities(self):
+        from models import storage as fs
+        from models.amenity import Amenity
+
+        amenity_objs = fs.all(Amenity)
+        place_amenities = []
+        for value in amenity_objs.values():
+            if value.id in self.amenity_ids:
+                place_amenities.append(value)
+        return place_amenities
+
+    @amenities.setter
+    def amenities(self, value):
+        from amenity import Amenity
+
+        if isinstance(value, Amenity):
+            self.amenity_ids.append(value.id)
