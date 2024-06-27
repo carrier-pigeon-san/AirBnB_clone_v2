4ce5c3ef91cf52e16b47d2e288a0860521dd068b
Sandy Obare <obaresandy@gmail.com>
2024-06-27 03:45:16 +0300

    * Addition of Base superclass to Amenity after BaseModel * Addition/Modification of class attributes to represent Table and columns

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This module contains the class Amenity that handles amenity objects
+that will be attributed to objects of Place class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """This class """
+    from models.place import place_amenity
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
