dd850d4dacef0184be8b112a02781600b54cfeb6
Sandy Obare <obaresandy@gmail.com>
2024-07-14 00:23:03 +0300

    2-do_deploy_web_static.py update #1 do_deploy() function logic and syntax update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,6 +2,7 @@
 """A Fabric script that generates a .tgz archive from the contents of the
 web_static folder of your AirBnB Clone repo, using the function do_pack.
 Distributes an archive to your web servers, using the function do_deploy"""
+import os
 from fabric.api import *
 from datetime import datetime
 
@@ -23,17 +24,26 @@ def do_deploy(archive_path):
     """Distributes an archive to your web servers"""
 
     env.hosts = ['100.26.223.74', '54.237.53.61']
-    if archive_path:
+
+    if not archive_path or not os.path.isfile(archive_path):
+        return False
+    
+    try:
+        file = archive_path.split('/')[-1]
+        file_no_ext = file.split('.')[0]
         releases = "/data/web_static/releases"
-        file = archive_path.split('/')[-1].split('.')[0]
-        result = local(f"ls {archive_path}")
-        if result.succeeded:
-            try:
-                put(archive_path, "/tmp/")
-                sudo(f"tar -xzvf /tmp/{archive_path} -C {releases}/{file}")
-                sudo(f"rm -rf /tmp/{archive_path.split('/')[0]}")
-                sudo(f"ln -sf {releases}/{file} /data/web_static/current")
-                return True
-            except Exception as e:
-                pass
-    return False
+        extract_path = f"{releases}/{file_no_ext}"
+
+        put((archive_path), f"/tmp/{file}")
+
+        run(f"tar -xzf /tmp/{file} -C {extract_path}")
+
+        run(f"rm /tmp/{file}")
+
+        run("rm -rf /data/web_static/current")
+
+        run(f"ln -s {extract_path} /data/web_static/current")
+
+        return True
+    except Exception as e:
+        return False
