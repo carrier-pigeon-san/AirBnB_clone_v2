f08c637e777d4a4fba0452f5e4b992591195162d
Sandy Obare <obaresandy@gmail.com>
2024-07-28 17:43:22 +0300

    3-python_route.py initial commit A script that starts a Flask web application Application routes: * /: displays 'Hello HBNB!' * /hbnb: displays 'HBNB' * /c/<text>: displays 'C' followed by the value of the 'text' variable (underscore '_' replaced with as space ' ') /python/<text>: displays 'Python', followed by the value of the 'text' variable (underscore '_' replaced with a space ' ')

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module contains a script that starts a Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """Returns a given string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """Returns a given string"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """Returns a given string through the variable in the route"""
+    return f"C {escape(text)}".replace("_", " ")
+
+
+@app.route("/python")
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """Returns a given string through the variable in the route"""
+    return f"Python {escape(text)}".replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
