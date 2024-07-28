1a426041200ac9a58d8d411d86871bcb803c7ae6
Sandy Obare <obaresandy@gmail.com>
2024-07-28 18:49:20 +0300

    4-number_route.py initial commit A script that starts a Flask web application Application route: * /number/<n>: displays '<n> is a number' only if n is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module contains a script that starts a Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """Returns a given string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """Returns a given string"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """Returns a given string through the variable in the route"""
+    return f"C {escape(text)}".replace("_", " ")
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """Returns a given string through the variable in the route"""
+    return f"Python {escape(text)}".replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    """Returns a the '<n> is  number' if n is an integer
+    """
+    if type(n) is int:
+        return f"{int(n)} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
