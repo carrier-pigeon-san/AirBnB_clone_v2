952db6b7150e61b5fc1a838ee57e3b007bdbf59b
Sandy Obare <obaresandy@gmail.com>
2024-07-29 00:56:12 +0300

    db_storage.py Addition of a public method def close(self):: calls remove() method on the private session attribute        (self.__session) or close() on the class Session

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -90,3 +90,8 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
+
+    def close(self):
+        """Calls remove() method on the private session attribute
+        (self.__session) or close() on the class Session"""
+        self.__session.close()
